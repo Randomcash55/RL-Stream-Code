2c6d2c5f0c47e5b37f3cfcc32587be07f328f855
Randomcash55 <Randomcash55@yahoo.com>
2021-04-21 14:34:30 -0400

    Update PressKey.py

--- PressKey.py
+++ PressKey.py
@@ -1,6 +1,6 @@
 import sys, winsound
 from pynput.keyboard import Key, Controller
-from time import time, sleep
+from time import sleep
 
 keyboard = Controller()
 
@@ -11,13 +11,15 @@ directionConversion = {
     "right": "d"
 }
 
+# Testing Beeps for Events
 frequency = 440
 duration = 500
 
+# Getting System Arguements
 direction = directionConversion[sys.argv[1]]
 holdTime = sys.argv[2]
 
-start = time()
+# Press and Hold Button
 winsound.Beep(frequency, duration)
 keyboard.press("a")
 sleep(holdTime)
